3e8a45b9e4930be06f81e28b2b8ccc4177527c6a
Monstre Charmant <ballonrage@gmail.com>
2021-07-22 21:00:33 +0200

    fix: add deps

--- build/django/entrypoint.sh
+++ build/django/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Start cronjob service(daemon)
-service cron start
+# service cron start
 
 # Run django in interactive mode
 if [ ${PYTHON_IS_PRODUCTION} = "true" ]; then
-    uwsgi --ini uwsgi.ini
+    _cmd="uwsgi --ini uwsgi.ini"
 else
-    python manage.py runserver 0.0.0.0:8000
+    _cmd="python manage.py runserver 0.0.0.0:8000"
 fi
+
+exec $_cmd
